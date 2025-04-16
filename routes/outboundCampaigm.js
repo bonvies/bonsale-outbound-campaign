a16ba0da8386b67103ff0d626a654f5a0629eb71
Aruminiya <leo80103@gmail.com>
2025-04-16 15:01:08 +0800

    feat: Optimize the process 1

--- routes/outboundCampaigm.js
+++ routes/outboundCampaigm.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const WebSocket = require('ws');
+const { set } = require('../app');
+const e = require('express');
 const router = express.Router();
 require('dotenv').config();
 
 const host = process.env.API_HOST;
 const wsHost = process.env.WS_HOST;
 
+// 取得 3CX token
 async function get3cxToken (grant_type, client_id, client_secret) {
   try {
     const response = await axios.post(`${host}/connect/token`, {
@@ -26,6 +29,7 @@ async function get3cxToken (grant_type, client_id, client_secret) {
   }
 };
 
+// 取得撥號者 讓 queue 去撥通電話
 async function getCaller (token) {
   try {
     const response = await axios.get(`${host}/callcontrol`, {
@@ -56,15 +60,31 @@ async function makeCall (token, dn, device_id, reason, destination, timeout = 30
         'Authorization': `Bearer ${token}`,
       }
     });
-
     // 回傳 API 的回應
-    return response
+    return response.data;
   } catch (error) {
     console.error('Error making API request:', error.message);
     throw new Error('Failed to makecall');
   }
 };
 
+// 取得參與者資訊 電話撥出時可用來抓取對方是否接聽
+async function getParticipants (token, dn) {
+  try {
+    const response = await axios.get(`${host}/callcontrol/${dn}/participants`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      }
+    });
+    console.log('參與者資訊：', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error making API request:', error.message);
+    throw new Error('Failed to get participants');
+  }
+};
+
+// 建立 WebSocket 連線 查看自動撥號狀態
 function callcontrolWs (token, phone, dn, device_id) {
   const phoneNumbersArray = phone.split(',');
   let nowCall = 0;
@@ -83,29 +103,41 @@ function callcontrolWs (token, phone, dn, device_id) {
       // ws.send(JSON.stringify({ message: 'Hello WebSocket Server!' }));
     });
 
-    ws.on('message', function message(data) {
+    ws.on('message', async function message(data) {
       try {
+        // 取得參與者資訊
+        const participants = await getParticipants(token, dn);
+        console.log('參與者資訊：', participants);
+
         // 將 Buffer 轉換為字串
         const messageString = data.toString();
     
         // 如果是 JSON 格式，嘗試解析
         const messageJson = JSON.parse(messageString);
     
-        console.log('Received JSON message from WebSocket server:', messageJson);
+        // console.log('Received JSON message from WebSocket server:', messageJson);
 
         const { event_type } = messageJson.event;
+        
         if (event_type === 1) {
           console.log('event_type:', event_type);
           nowCall++;
-          console.log(`前一隻手機掛斷了 現在要撥給 第 ${nowCall + 1} 隻手機: ${phoneNumbersArray[nowCall]}`);
-          makeCall(token, dn, device_id, 'outbound', phoneNumbersArray[nowCall]);
+          console.log(`前一隻手機掛斷了 5秒後準備撥給 第 ${nowCall + 1} 隻手機: ${phoneNumbersArray[nowCall]}`);
+
+          if (!phoneNumbersArray[nowCall]) {
+            console.log('No more phone numbers to call');
+            nowCall = 0; // 重置計數器
+            ws.close(); // 關閉 WebSocket 連線
+            return;
+          } else {
+            // 等待 5 秒後撥打下一個電話
+            setTimeout(() => {
+              makeCall(token, dn, device_id, 'outbound', phoneNumbersArray[nowCall]);
+            }, 5000);
+          }
+
         }
 
-        if (!phoneNumbersArray[nowCall]) {
-          console.log('No more phone numbers to call');
-          nowCall = 0; // 重置計數器
-          ws.close(); // 關閉 WebSocket 連線
-        }
       } catch (error) {
         // 如果不是 JSON 格式，直接輸出字串
         console.log('Received raw message from WebSocket server:', data.toString());
@@ -127,7 +159,7 @@ function callcontrolWs (token, phone, dn, device_id) {
   }
 };
 
-/* GET outboundCampaigm listing. */
+// 主要 的 API
 router.post('/', async function(req, res, next) {
   const { grant_type, client_id, client_secret, phone } = req.body;
 
