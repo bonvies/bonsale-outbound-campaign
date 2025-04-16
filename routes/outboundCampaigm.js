b8edcf264aa3f29c41dbe899710ae56ff93ec31f
Aruminiya <leo80103@gmail.com>
2025-04-16 15:43:25 +0800

    feat: optimize the process 2

--- routes/outboundCampaigm.js
+++ routes/outboundCampaigm.js
@@ -1,42 +1,45 @@
 const express = require('express');
 const axios = require('axios');
 const WebSocket = require('ws');
-const { set } = require('../app');
-const e = require('express');
 const router = express.Router();
 require('dotenv').config();
 
 const host = process.env.API_HOST;
 const wsHost = process.env.WS_HOST;
 
+// 創建一個 axios 實例
+const axiosInstance = axios.create({
+  baseURL: host, // 你的 API 基礎 URL
+});
+
 // 取得 3CX token
 async function get3cxToken (grant_type, client_id, client_secret) {
   try {
-    const response = await axios.post(`${host}/connect/token`, {
-      grant_type,
-      client_id,
-      client_secret
-    }, {
+    const params = new URLSearchParams();
+    params.append('grant_type', grant_type);
+    params.append('client_id', client_id);
+    params.append('client_secret', client_secret);
+
+    const response = await axiosInstance.post('/connect/token', params, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
     });
 
+    // 設定預設的 Authorization 標頭
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`;
+
     return response.data.access_token;
   } catch (error) {
-  console.error('Error making API request:', error.message);
+  console.error('Error get3cxToken request:', error.message);
   throw new Error('Failed to fetch token');
   }
 };
 
 // 取得撥號者 讓 queue 去撥通電話
-async function getCaller (token) {
+async function getCaller () {
   try {
-    const response = await axios.get(`${host}/callcontrol`, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      }
-    });
+    const response = await axiosInstance.get('/callcontrol');
     const caller = response.data.find(item => item.type === 'Wqueue');
     if (!caller) {
       throw new Error('Caller not found');
@@ -44,42 +47,34 @@ async function getCaller (token) {
 
     return caller;
   } catch (error) {
-    console.error('Error making API request:', error.message);
+    console.error('Error getCaller request:', error.message);
     throw new Error('Failed to getCaller data');
   } 
 };
 
-async function makeCall (token, dn, device_id, reason, destination, timeout = 30) {
+async function makeCall (dn, device_id, reason, destination, timeout = 30) {
   try {
-    const response = await axios.post(`${host}/callcontrol/${dn}/devices/${device_id}/makecall`, {
+    const response = await axiosInstance.post(`/callcontrol/${dn}/devices/${device_id}/makecall`, {
       reason,
       destination,
       timeout
-    }, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      }
     });
     // 回傳 API 的回應
     return response.data;
   } catch (error) {
-    console.error('Error making API request:', error.message);
+    console.error('Error makeCall request:', error.message);
     throw new Error('Failed to makecall');
   }
 };
 
 // 取得參與者資訊 電話撥出時可用來抓取對方是否接聽
-async function getParticipants (token, dn) {
+async function getParticipants (dn) {
   try {
-    const response = await axios.get(`${host}/callcontrol/${dn}/participants`, {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      }
-    });
+    const response = await axiosInstance.get(`/callcontrol/${dn}/participants`);
     console.log('參與者資訊：', response.data);
     return response.data;
   } catch (error) {
-    console.error('Error making API request:', error.message);
+    console.error('Error getParticipants request:', error.message);
     throw new Error('Failed to get participants');
   }
 };
@@ -106,8 +101,8 @@ function callcontrolWs (token, phone, dn, device_id) {
     ws.on('message', async function message(data) {
       try {
         // 取得參與者資訊
-        const participants = await getParticipants(token, dn);
-        console.log('參與者資訊：', participants);
+        const participants = await getParticipants(dn);
+        // console.log('參與者資訊 : ', participants);
 
         // 將 Buffer 轉換為字串
         const messageString = data.toString();
@@ -115,13 +110,22 @@ function callcontrolWs (token, phone, dn, device_id) {
         // 如果是 JSON 格式，嘗試解析
         const messageJson = JSON.parse(messageString);
     
-        // console.log('Received JSON message from WebSocket server:', messageJson);
+        // console.log('WebSocket server 接收數據 : ', messageJson);
 
         const { event_type } = messageJson.event;
+
+        // 整合 參與者資訊 和 WebSocket server 接收數據
+        const resultData = {
+          event_type: event_type,
+          event: messageJson.event,
+          participants: participants
+        }
+        console.log('整合 參與者資訊 和 WebSocket server 接收數據 : ', resultData);
         
         if (event_type === 1) {
           console.log('event_type:', event_type);
           nowCall++;
+          console.log('=================== 我是分隔線 ====================');
           console.log(`前一隻手機掛斷了 5秒後準備撥給 第 ${nowCall + 1} 隻手機: ${phoneNumbersArray[nowCall]}`);
 
           if (!phoneNumbersArray[nowCall]) {
@@ -132,7 +136,7 @@ function callcontrolWs (token, phone, dn, device_id) {
           } else {
             // 等待 5 秒後撥打下一個電話
             setTimeout(() => {
-              makeCall(token, dn, device_id, 'outbound', phoneNumbersArray[nowCall]);
+              makeCall(dn, device_id, 'outbound', phoneNumbersArray[nowCall]);
             }, 5000);
           }
 
@@ -171,15 +175,19 @@ router.post('/', async function(req, res, next) {
     const token = await get3cxToken(grant_type, client_id, client_secret);
     console.log(token);
     // 取得 撥號分機資訊 (需要設定 queue)
-    const caller = await getCaller(token);
+    const caller = await getCaller();
     const { dn, device_id } = caller.devices[0]; // TODO 這邊我只有取第一台設備資訊
 
     // 進行初次撥打電話
     const phoneNumbersArray = phone.split(',');
-    makeCall(token, dn, device_id, 'outbound', phoneNumbersArray[0]);
+    makeCall(dn, device_id, 'outbound', phoneNumbersArray[0]);
 
     // 建立 WebSocket 連線
-    callcontrolWs(token, phone, dn, device_id);
+    try {
+      callcontrolWs(token, phone, dn, device_id);
+    } catch (error) {
+      console.error('Error establishing WebSocket connection:', error.message);
+    }
 
     // Log the received data (for debugging purposes)
     console.log({ grant_type, client_id, client_secret, phone });
